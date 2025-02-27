69f11ba4f102f2829bb454874ab6636405b370fa
maryammrg <maryamelouadaa@gamil.com>
2025-02-27 15:22:18 +0000

    login Servlet

--- src/main/java/org/example/edumanage/servlets/LoginServlet.java
+++ src/main/java/org/example/edumanage/servlets/LoginServlet.java
@@ -1,4 +1,35 @@
 package org.example.edumanage.servlets;
 
-public class LoginServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.edumanage.Dao.UserDao;
+import org.example.edumanage.bean.User;
+
+import java.io.IOException;
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private UserDao userDao;
+    public void init() {userDao = new UserDao();}
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.sendRedirect("login.jsp");
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        User user=userDao.getUser(username,password);
+        if(user!=null){
+            request.setAttribute("user",user);
+            request.getRequestDispatcher("Home.jsp").forward(request,response);
+        }
+        else{
+            response.sendRedirect("login.jsp");
+        }
+
+    }
 }
